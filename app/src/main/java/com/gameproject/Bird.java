103c5ed5b7995e028758e4e3c73a4bf8a2e849e7
Vipr <viper.ca082@outlook.com>
2025-03-13 12:32:00 -0600

    Declared bird vars

--- app/src/main/java/com/gameproject/Bird.java
+++ app/src/main/java/com/gameproject/Bird.java
@@ -1,4 +1,14 @@
 package com.gameproject;
 
+import android.media.Image;
+
 public class Bird {
+    //vars
+    int birdHeight;
+    int birdWidth;
+    int birdX;
+    int birdY;
+    boolean isDead;
+    Image skin;
+
 }
