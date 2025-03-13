664ef471231990b7f72ac50d1707f2bed8f45296
Vipr <viper.ca082@outlook.com>
2025-03-13 12:13:09 -0600

    Started bird class

--- app/src/main/java/com/gameproject/MainActivity.java
+++ app/src/main/java/com/gameproject/MainActivity.java
@@ -21,4 +21,10 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+    /**
+    Initialize the game
+     */
+    protected void mainGame(){
+
+    }
 }
