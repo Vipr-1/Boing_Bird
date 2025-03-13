5982bc096271702720f122a055efba2560a14618
Vipr <viper.ca082@outlook.com>
2025-03-13 13:17:32 -0600

    Added Java doc comments

--- app/src/main/java/com/gameproject/Plant.java
+++ app/src/main/java/com/gameproject/Plant.java
@@ -12,12 +12,29 @@ public class Plant {
     int plantY;
 
     //methods
+
+    /**
+     * Checks if the bird has collided with a plant
+     * @param birdX -The X coordinate of the bird
+     * @param birdY -The Y coordinate of the bird
+     * @param birdWidth -The bird's width value, used for extending X for hitbox purposes
+     * @param birdHeight -The bird's width value, used for extending Y for hitbox purposes
+     * @return -If a collision has occurred, true will be returned, else it will return false
+     */
     boolean collidePlant(int birdX, int birdY, int birdWidth, int birdHeight){
 
     }
+
+    /**
+     * updates plants attributes
+     */
     void update(){
 
     }
+
+    /**
+     * resets plant to default state
+     */
     void reset(){
 
     }
